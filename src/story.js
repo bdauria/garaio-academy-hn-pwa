7897cf9b4bf85fcc6f572755b965fa73d5d76816
Bruno <bruno.dauria1@gmail.com>
2017-10-13 15:09:05 +0200

    Change Story to use JSS and make stateless Component.

--- src/story.js
+++ src/story.js
@@ -1,100 +1,62 @@
 import { h, Component } from 'preact';
-import styled from 'styled-components';
-import { connect } from 'preact-redux';
-import { fetchStory } from './hacker-news-api';
-import { loadStory } from './stories-reducer';
-import moment from 'moment';
-import Pluralize from 'pluralize';
-import { CircularProgress } from 'material-ui/Progress';
+import withStyles from 'material-ui/styles/withStyles';
 
-const Post = styled.li`
-  border-bottom: 1px solid #ccc;
-  margin: 0px 30px 0 30px;
-  padding: 10px 0 10px 5px;
-`;
-
-const StoryTitle = styled.a`
-  color: #484848;
-  text-decoration: none;
-  cursor: pointer;
-  font-size: 18px;
-`;
-
-const SubInfo = styled.a`
-  color: #808080;
-  margin-left: 5px;
-  margin-right: 5px;
-`;
-
-const StoryInfo = styled.div`
-  font-size: 15px;
-  padding: 0;
-`;
-
-const domain = url => {
-  if (url === undefined) {
-    return '';
+const styles = {
+  post: {
+    borderBottom: '1px solid #ccc',
+    marginRight: '40px',
+    padding: '10px 0 10px 0px'
+  },
+  storyTitle: {
+    color: '#484848',
+    textDecoration: 'none',
+    cursor: 'pointer',
+    fontSize: '18px'
+  },
+  subInfo: {
+    color: '#808080',
+    marginLeft: '5px',
+    marginRight: '5px'
+  },
+  storyInfo: {
+    fontSize: '15px',
+    padding: '0'
   }
-  const domain = '(' + url.split('/')[2] + ')';
-  return domain ? domain.replace('www.', '') : '';
 };
 
-const commentsString = descendants => {
-  if (!descendants || descendants === 0) {
+const commentsString = count => {
+  if (!count || count === 0) {
     return 'discuss';
   }
-  return `${descendants} ${Pluralize('comment', descendants)}`;
+  return count > 1 ? 'comments' : 'comment';
 };
 
 class Story extends Component {
-  componentDidMount() {
-    this.props.fetchStory(this.props.type, this.props.id);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.id === this.props.id) {
-      return;
-    }
-    this.props.fetchStory(nextProps.type, nextProps.id);
-  }
-
   content() {
-    if (!this.props.story.id) {
-      return <CircularProgress />;
-    }
+    const { classes } = this.props;
     return (
       <div>
         <div>
-          <StoryTitle href={this.props.story.url}>
+          <a className={classes.storyTitle} href={this.props.story.url}>
             {this.props.story.title}
-          </StoryTitle>
-          <SubInfo>{domain(this.props.story.url)}</SubInfo>
+          </a>
+          <a className={classes.subInfo}>{this.props.story.domain}</a>
         </div>
-        <StoryInfo>
-          <SubInfo>{this.props.story.score} points by</SubInfo>
-          {this.props.story.by}
-          <SubInfo>{moment.unix(this.props.story.time).fromNow()}</SubInfo>
+        <a className={classes.storyInfo}>
+          <a className={classes.subInfo}>{this.props.story.points} points by</a>
+          {this.props.story.user}
+          <a className={classes.subInfo}>{this.props.story.time_ago}</a>
           {'| '}
-          {commentsString(this.props.story.descendants)}
-        </StoryInfo>{' '}
+          {commentsString(this.props.story.comments_count)}
+        </a>{' '}
       </div>
     );
   }
 
   render() {
-    return <Post>{this.content()}</Post>;
+    const { classes } = this.props;
+    return <li className={classes.post}>{this.content()}</li>;
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return { story: { ...state.stories[ownProps.type].byId[ownProps.id] } };
-};
-
-const mapDispatchToProps = dispatch => ({
-  fetchStory: (type, id) =>
-    fetchStory(id).then(response => {
-      return dispatch(loadStory(type, id, response.data));
-    })
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Story);
+export default withStyles(styles)(Story);
