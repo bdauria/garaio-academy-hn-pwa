252aeff88f02b683e05ad8ae8282e5f1ec7b17d6
Bruno <bruno.dauria1@gmail.com>
2017-10-13 15:05:22 +0200

    Webpack config for generating SSR Function.

--- webpack.config.js
+++ webpack.config.js
@@ -3,78 +3,90 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const webpack = require('webpack');
 
-module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/font-woff'
-          }
-        }
-      },
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'application/font-woff'
-          }
-        }
-      }
-    ]
-  },
-  resolve: {
-    modules: [path.join(__dirname, 'node_modules')],
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat'
-    }
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: 'src/global.css'
-      }
-    ]),
-    new HtmlWebpackIncludeAssetsPlugin({
-      assets: ['global.css'],
-      append: false
-    }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
-      meta: [
+module.exports = [
+  {
+    name: 'client',
+    entry: ['./src/index.js'],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
         {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0, user-scalable=no'
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
       ]
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    historyApiFallback: true
+    },
+    resolve: {
+      modules: [path.join(__dirname, 'node_modules')],
+      alias: {
+        react: 'preact-compat',
+        'react-dom': 'preact-compat'
+      }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          process.env.NODE_ENV || 'development'
+        )
+      }),
+      new MinifyPlugin(),
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: require('html-webpack-template'),
+        meta: [
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1.0, user-scalable=no'
+          }
+        ]
+      })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      historyApiFallback: true
+    }
+  },
+  {
+    name: 'server',
+    entry: './src/functions.js',
+    target: 'node',
+    output: {
+      library: '',
+      libraryTarget: 'commonjs',
+      path: path.join(__dirname, 'functions'),
+      publicPath: '/',
+      filename: 'index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      modules: [path.join(__dirname, 'node_modules')],
+      alias: {
+        react: 'preact-compat',
+        'react-dom': 'preact-compat'
+      }
+    },
+    externals: [require('webpack-node-externals')()]
   }
-};
+];
