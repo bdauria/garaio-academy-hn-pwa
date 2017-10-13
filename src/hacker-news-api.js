4192d9cba2851c72d6dd439d08fd90b7ea1ceaf7
Bruno <bruno.dauria1@gmail.com>
2017-10-13 15:06:56 +0200

    Use unofficial HN Api.

--- src/hacker-news-api.js
+++ src/hacker-news-api.js
@@ -1,6 +1,7 @@
 import { get } from 'axios';
-const baseUrl = 'https://hacker-news.firebaseio.com/v0';
+const baseUrl = 'https://node-hnapi.herokuapp.com';
 
-export const fetchStories = type => get(`${baseUrl}/${type}stories.json`);
+export const fetchStories = (type, page) =>
+  get(`${baseUrl}/${type}?page=${page}`);
 
 export const fetchStory = id => get(`${baseUrl}/item/${id}.json`);
