8847777e3f0ceb09048174e6532f350f53a88120
Bruno <bruno.dauria1@gmail.com>
2017-10-18 17:56:16 +0200

    Remove not used fetchStory from api

--- src/hacker-news-api.js
+++ src/hacker-news-api.js
@@ -3,5 +3,3 @@ const baseUrl = 'https://node-hnapi.herokuapp.com';
 
 export const fetchStories = (type, page) =>
   get(`${baseUrl}/${type}?page=${page}`);
-
-export const fetchStory = id => get(`${baseUrl}/item/${id}.json`);
